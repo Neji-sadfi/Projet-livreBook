4bc8cce6c059a37ec080a0b9bdc0d2d0770893a9
Neji-sadfi <mohamednejisadfi@gmail.com>
2024-01-20 11:42:21 +0100

    j'ai ajouté la classe de test

--- src/main/java/com/example/livrebook/test/actuality/ActualityAdminView.java
+++ src/main/java/com/example/livrebook/test/actuality/ActualityAdminView.java
@@ -1,17 +1,24 @@
-package com.example.livrebook.test;
+package com.example.livrebook.test.actuality;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class ActualityAdminView extends Application {
     @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/livrebook/Actuality-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) throws IOException {
+            FXMLLoader fxmlLoader = new FXMLLoader(ActualityAdminView.class.getResource("/com/example/livrebook/actuality/Actuality-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+            stage.setTitle("Ajout actualité");
+            stage.setScene(scene);
+            stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
